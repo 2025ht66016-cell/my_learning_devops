c9710de9c973b07705fcaea679e0a8f150e8a3e3
2025ht66016-cell <2025ht66016@wilp.bits-pilani.ac.in>
2026-04-03 13:59:22 +0530

    test_app.py has been updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import create_app
 from src.Aceestver import get_program_by_code, get_programs_summary
 import sys, os
